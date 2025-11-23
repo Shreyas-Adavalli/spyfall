00b632ce7ea63112cbe1746ef2e0540484451936
Shreyas-Adavalli <shreyas.adavalli@gmail.com>
2025-11-23 21:12:46 +1100

    Corrected debug

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import random
 from categories import CATEGORIES
+import os
 
 app = Flask(__name__)
 
@@ -90,4 +91,5 @@ def game():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
