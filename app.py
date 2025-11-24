c28ed0f13f304eaaf500c54154e10d1d68bf1c76
Shreyas-Adavalli <shreyas.adavalli@gmail.com>
2025-11-24 18:48:19 +1100

    debug

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def game():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
